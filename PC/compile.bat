0a314c7a6195cb031db6120503ee517ffc6651aa
MrUsername7 <75948302+MrUsername7@users.noreply.github.com>
2026-09-05 19:43:45 +0200

    compiling stuff

--- PC/compile.bat
+++ PC/compile.bat
@@ -1,1 +1,3 @@
-pyinstaller --clean --onefile --noconsole --optimize 2 --add-data="assets:assets" "Meteor Shooter.py"
+echo off
+py -3.12 -m PyInstaller --clean --onefile --noconsole --optimize 2 --add-data="assets:assets" "Meteor Shooter.py"
+echo on
