243ab0be8c564a13dc70d2d5c78d86b591ec260b
MrUsername7 <75948302+MrUsername7@users.noreply.github.com>
2026-09-05 19:11:35 +0200

    Update compile.bat

--- PC/compile.bat
+++ PC/compile.bat
@@ -1,1 +1,1 @@
-python3 -m PyInstaller --clean --onefile --noconsole --optimize 2 --add-data="assets:assets" "Meteor Shooter.py"
+pyinstaller --clean --onefile --noconsole --optimize 2 --add-data="assets:assets" "Meteor Shooter.py"
